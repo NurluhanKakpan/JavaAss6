6bd4c8fff75cfa5fc609273ec8ba5dd5be8f397d
NurluhanKakpan <nurluhankakpan@gmail.com>
2023-05-31 23:19:22 +0600

    I create class Vertex<V>

---
+++ src/Vertex.java
@@ -0,0 +1,23 @@
+import java.util.Map;
+
+public class Vertex<V> {
+    private V data;
+
+    private Map<Vertex<V>, Double> adjacentVertices;
+
+    public V getData() {
+        return data;
+    }
+
+    public void addAdjacentVertex(Vertex<V> destination, double weight) {
+        adjacentVertices.put(destination, weight);
+    }
+
+    public Map<Vertex<V>, Double> getAdjacentVertices() {
+        return adjacentVertices;
+    }
+
+    public String toString() {
+        return String.valueOf(data);
+    }
+}
